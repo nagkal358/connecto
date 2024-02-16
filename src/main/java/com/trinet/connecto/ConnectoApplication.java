ed05ac01e9b09069958de47eb2db063b02edc073
nageswara_cg <nagkal358@gmail.com>
2024-02-16 20:25:19 +0530

    initial commit

---
+++ src/main/java/com/trinet/connecto/ConnectoApplication.java
@@ -0,0 +1,13 @@
+package com.trinet.connecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trinet/connecto/ConnectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinet.connecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
