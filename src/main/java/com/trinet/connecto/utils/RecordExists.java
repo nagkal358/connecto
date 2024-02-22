ba248bf674b4e34324edc4ecb9bf52d24ef3d509
nageswara_cg <nagkal358@gmail.com>
2024-02-23 00:58:07 +0530

    added new api for getting counts by status and also added duplicate check for employee in sign up with email

--- src/main/java/com/trinet/connecto/utils/RecordExists.java
+++ src/main/java/com/trinet/connecto/utils/RecordExists.java
@@ -1,6 +1,5 @@
 package com.trinet.connecto.utils;
 
-import org.bson.json.JsonObject;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -11,6 +10,7 @@ public class RecordExists {
     public Map<String, String> recordExists(String recordName) {
         Map<String, String> m = new HashMap<>();
         m.put("message", recordName + " Already Exists");
+        m.put("ERROR", "409");
         return m;
     }
 }
